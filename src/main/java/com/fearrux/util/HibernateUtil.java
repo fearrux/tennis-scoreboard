ddeb6be488f102b162c8097f9df5fd6d7764648c
fearrux <tembulatbekov7@gmail.com>
2025-02-09 21:01:21 +0300

    feat: add utility class for get SessionFactory

---
+++ src/main/java/com/fearrux/util/HibernateUtil.java
@@ -0,0 +1,14 @@
+package com.fearrux.util;
+
+import lombok.experimental.UtilityClass;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@UtilityClass
+public class HibernateUtil {
+    public static SessionFactory get() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        return configuration.buildSessionFactory();
+    }
+}
