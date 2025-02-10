6cbe86dc43014c30206abc93acc0a0f7ae64e0f3
fearrux <tembulatbekov7@gmail.com>
2025-02-10 20:37:38 +0300

    feat: add custom exception when match validate is fail

---
+++ src/main/java/com/fearrux/exception/InvalidMatchException.java
@@ -0,0 +1,7 @@
+package com.fearrux.exception;
+
+public class InvalidMatchException extends RuntimeException {
+    public InvalidMatchException(String message) {
+        super(message);
+    }
+}
