0e1b5c2875727d8d9dff7b355e07f20538dc573f
fearrux <tembulatbekov7@gmail.com>
2025-02-12 19:11:53 +0300

    add implementation of player repository

---
+++ src/main/java/com/fearrux/repository/HibernatePlayerRepository.java
@@ -0,0 +1,33 @@
+package com.fearrux.repository;
+
+import com.fearrux.entity.Player;
+import com.fearrux.util.HibernateUtil;
+import org.hibernate.Session;
+
+import java.util.Optional;
+
+public class HibernatePlayerRepository implements PlayerRepository {
+    @Override
+    public Optional<Player> findByName(String name) {
+        try (Session session = HibernateUtil.get().openSession()) {
+            return session.createQuery("FROM Player p WHERE p.name = :name", Player.class)
+                    .setParameter("name", name)
+                    .uniqueResultOptional();
+        }
+    }
+
+    @Override
+    public Optional<Player> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Player save(Player player) {
+        try (Session session = HibernateUtil.get().openSession()) {
+            session.beginTransaction();
+            session.persist(player);
+            session.getTransaction().commit();
+            return player;
+        }
+    }
+}
