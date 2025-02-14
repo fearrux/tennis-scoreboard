f42dd03cddb73ca2efd514857089881dae0b8959
fearrux <tembulatbekov7@gmail.com>
2025-02-14 21:49:55 +0300

    add isMatchOver block

--- src/main/java/com/fearrux/servlet/MatchScoreServlet.java
+++ src/main/java/com/fearrux/servlet/MatchScoreServlet.java
@@ -2,6 +2,7 @@ package com.fearrux.servlet;
 
 import com.fearrux.dto.MatchRequestDTO;
 import com.fearrux.exception.InvalidUuidException;
+import com.fearrux.service.FinishedMatchesPersistenceService;
 import com.fearrux.service.MatchScoreCalculationService;
 import com.fearrux.service.OngoingMatchesService;
 import com.fearrux.util.JSPHelper;
@@ -12,22 +13,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.util.Objects;
 import java.util.UUID;
 
 @WebServlet(name = "match-score", value = "/match-score")
 public class MatchScoreServlet extends HttpServlet {
     private final OngoingMatchesService ongoingMatchesService = OngoingMatchesService.getInstance();
     private final MatchScoreCalculationService matchScoreCalculationService = new MatchScoreCalculationService();
+    private final FinishedMatchesPersistenceService finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String uuid = req.getParameter("uuid");
-
         MatchRequestDTO matchRequestDTO = ongoingMatchesService.get(UUID.fromString(uuid));
-
         req.setAttribute("match", matchRequestDTO);
-
         req.getRequestDispatcher(JSPHelper.getPath("match-score")).forward(req, resp);
     }
 
@@ -36,23 +34,27 @@ public class MatchScoreServlet extends HttpServlet {
         String uuid = req.getParameter("uuid");
         String playerId = req.getParameter("player_id");
 
-        isValidUuid(uuid);
-        MatchRequestDTO matchRequestDTO = ongoingMatchesService.get(UUID.fromString(uuid));
+        UUID validUuid = isValidUuid(uuid);
+        MatchRequestDTO matchRequestDTO = ongoingMatchesService.get(validUuid);
         matchScoreCalculationService.updateScore(matchRequestDTO, Long.parseLong(playerId));
 
+        if (matchScoreCalculationService.isMatchOver(matchRequestDTO.getMatchState())) {
+            ongoingMatchesService.remove(validUuid);
+            finishedMatchesPersistenceService.save(matchRequestDTO, matchScoreCalculationService.getWinner(matchRequestDTO));
+        }
         req.setAttribute("match", matchRequestDTO);
-
         req.getRequestDispatcher(JSPHelper.getPath("match-score")).forward(req, resp);
     }
 
-    private void isValidUuid(String uuid) {
+    private UUID isValidUuid(String uuid) {
         if (uuid.isEmpty()) {
             throw new InvalidUuidException("UUID cannot be empty.");
         }
         try {
-            UUID.fromString(uuid);
+            return UUID.fromString(uuid);
         } catch (IllegalArgumentException exception) {
             throw new InvalidUuidException("Invalid UUID format. Expected: 8-4-4-4-12.");
         }
     }
+
 }
