bd785b8255ae57e1fb308e585e0b26f7d87c435d
fearrux <tembulatbekov7@gmail.com>
2025-02-14 21:45:02 +0300

    add JoinColumn annotation

--- src/main/java/com/fearrux/entity/Match.java
+++ src/main/java/com/fearrux/entity/Match.java
@@ -14,10 +14,16 @@ public class Match {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
+    @JoinColumn(name = "first_player_id")
     private Player firstPlayer;
+
     @ManyToOne
+    @JoinColumn(name = "second_player_id")
     private Player secondPlayer;
+
     @ManyToOne
+    @JoinColumn(name = "winner_id")
     private Player winner;
 }
