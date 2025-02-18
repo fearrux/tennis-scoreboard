95feded15ccfbf6c9e0966ceb89b467ffa6d6b53
fearrux <tembulatbekov7@gmail.com>
2025-02-18 20:53:13 +0300

    update findById method, change query in findByName method

--- src/main/java/com/fearrux/repository/HibernatePlayerRepository.java
+++ src/main/java/com/fearrux/repository/HibernatePlayerRepository.java
@@ -7,18 +7,24 @@ import org.hibernate.Session;
 import java.util.Optional;
 
 public class HibernatePlayerRepository implements PlayerRepository {
+
     @Override
     public Optional<Player> findByName(String name) {
         try (Session session = HibernateUtil.get().openSession()) {
-            return session.createQuery("FROM Player p WHERE p.name = :name", Player.class)
+            return session.createQuery(
+                            "FROM Player player WHERE player.name = :name",
+                            Player.class
+                    )
                     .setParameter("name", name)
                     .uniqueResultOptional();
         }
     }
 
     @Override
-    public Optional<Player> findById(Long aLong) {
-        return Optional.empty();
+    public Optional<Player> findById(Long id) {
+        try (Session session = HibernateUtil.get().openSession()) {
+            return Optional.ofNullable(session.find(Player.class, id));
+        }
     }
 
     @Override
