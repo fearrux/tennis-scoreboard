5be0bf8f61508b7b803efa44279e4cb16059d0d2
fearrux <tembulatbekov7@gmail.com>
2025-02-18 20:54:00 +0300

    use stream instead value of to joining errors in list with separator

--- src/main/java/com/fearrux/exception/InvalidNameException.java
+++ src/main/java/com/fearrux/exception/InvalidNameException.java
@@ -3,9 +3,11 @@ package com.fearrux.exception;
 import com.fearrux.validator.Error;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class InvalidNameException extends RuntimeException {
+
     public InvalidNameException(List<Error> errors) {
-        super(String.valueOf(errors));
+        super(errors.stream().map(String::valueOf).collect(Collectors.joining(", ")));
     }
 }
